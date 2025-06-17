d4c75bb4ef4a7f57db5d7a38cccb06a354c34e0b
Lalitha devi <22lalithadevi@gmail.com>
2025-06-17 09:53:56 +0530

    Update student.py

--- Oops/student.py
+++ Oops/student.py
@@ -5,7 +5,15 @@ class student:
         self.mat_marks=mat_marks
         self.phy_marks=phy_marks
         self.che_marks=che_marks
-class derived(marks):
-    def __init__(self,mat_marks,phy_marks,che_marks):
-        marks.__init__(self)   
-        print(self.mat_marks+phy_marks+che_marks)    
+    def cal(self):
+        if(self.mat_marks>=40 and self.phy_marks>=40 and self.che_marks>=40):
+            total= self.mat_marks+self.phy_marks+self.che_marks
+            avg=total/3
+            print(total)
+            print(avg)
+            if(self.mat_marks>75 or self.phy_marks>75) or (self.phy_marks>75 and self.che_marks>75) or (self.che_marks>75 and self.mat_marks>75):
+                print("admission is confirmrd..!")
+            else:
+                print("admission is not confirmrd....!")
+s1=student("lalitha",148,99,98,99)
+s1.cal()
