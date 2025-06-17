39e337a2e6c99901dc9068b22d699559cecdeb36
Lalitha devi <22lalithadevi@gmail.com>
2025-06-17 11:11:02 +0530

    Update and rename multilevel.py to multilevel_hierarchy.py

--- Oops/multilevel_hierarchy.py
+++ Oops/multilevel_hierarchy.py
@@ -26,3 +26,21 @@ p1=peacock()
 p1.dancing()
 p1.flying()
 p1.sound()
+
+
+# example for hierarical
+class shape:
+    def area(self):
+        print('calculate area..!')
+class circle(shape):
+    def circlearea(self,radius):
+        print('area of circle: ',3.14*radius*radius)
+class square(shape):
+    def squarearea(self,side):
+        print("area of square: ",side*side)
+c1=circle()
+c1.area()
+c1.circlearea(4)
+s1=square()
+s1.area()
+s1.squarearea(5)
